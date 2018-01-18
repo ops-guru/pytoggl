dbe4135373e5edde2bb0dec4a08943b1b14c4181
Max Kovgan <max@opsguru.io>
2018-01-18 20:16:39 +0200

    fix how package is built
    - fix how README is linked
    
    Signed-off-by: Max Kovgan <max@opsguru.io>

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env py
-
+import os
 from setuptools import setup
+LINK_NAME = 'README'
+if not os.path.exists(LINK_NAME):
+    os.symlink('.'.join([LINK_NAME, 'md']), LINK_NAME)
 setup(
     setup_requires=['pbr'],
     pbr=True,
